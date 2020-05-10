60da8dc1caf007e1413bbff3796da989422cb8c2
cheesepencil <cheesepencil@cheesepencil.com>
2020-05-10 18:50:18 -0400

    ongoing work on maze algo

--- forest.ts
+++ forest.ts
@@ -16,14 +16,18 @@ export class Forest {
         }
 
         let here = this.getNode(4, 4);
+        let visitedNodes: ForestNode[] = [];
         let walking = true;
-        while (walking) {
 
-            let direction = this.directions[Phaser.Math.Between(0, 3)];
-            if ("north") {
-
+        while (true) {
+            let candidateNodes = this.getUnvisitedNeighbors(here, visitedNodes);
+            if (candidateNodes.length === 0){
+                break;
             }
-            break;
+
+            let nextNode = candidateNodes[Phaser.Math.Between(0,candidateNodes.length)];
+            
+            
         }
     }
 
@@ -31,6 +35,34 @@ export class Forest {
         return this.nodes.filter((f) => {
             return f.x === x && f.y === y;
         })[0];
+    }
+
+    getUnvisitedNeighbors(here: ForestNode, visitedNodes: ForestNode[]): ForestNode[] {
+        let neighbors = this.getNeighbors(here);
+
+        let result = neighbors.filter(n => {
+            return visitedNodes.filter(v => {
+                return v.x === n.x && v.y === n.y;
+            }).length === 0;
+        });
+
+        return result;
+    }
+
+    getNeighbors(here: ForestNode): ForestNode[] {
+        let modifiers = [-1, 1];
+        let results: ForestNode[] = [];
+
+        for (let i = 0; i < modifiers.length; i++) {
+            for (let j = 0; j < modifiers.length; j++) {
+                let node = this.getNode(here.x + i, here.y + j);
+                if (node) {
+                    results.push(node);
+                }
+            }
+        }
+
+        return results;
     }
 
     getOppositeDirection(direction: string): string {
