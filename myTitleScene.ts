9e29fbf7844e8492cd50b1e9afadb364d5d6e70a
Arun Mahadevan <arungm29@gmail.com>
2020-05-17 05:50:20 +0530

    Fixed merge error

--- myTitleScene.ts
+++ myTitleScene.ts
@@ -1,11 +1,7 @@
 export class MyTitleScene extends Phaser.Scene {
     constructor() {
         super({
-<<<<<<< HEAD
-            key: "MyTitleScene"
-=======
             key: "MyTitleScene",
->>>>>>> upstream/master
         });
     }
 
@@ -13,11 +9,7 @@ export class MyTitleScene extends Phaser.Scene {
         //this.load.image('loading', require('./assets/loading.png'));
     }
 
-<<<<<<< HEAD
-    create() {
-=======
     create(): void {
->>>>>>> upstream/master
         this.scene.start("MyGameScene");
     }
 }
